36329e15c538346edcbf9dd7f3e99f9f1a30de0b
Kris <git@k118.de>
2020-03-20 21:31:54 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
+############## CONFIG ###############
+core_instance="xxx.de"
+bssid_area="xx:xx:xx"
+scanning_interface="wlan0"
+#####################################
+
 from wifi import Cell, Scheme
 import datetime
 import time
@@ -6,16 +12,19 @@ import requests
 found = []
 
 while True:
-	try:
-		print(datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + ": Scan...")
-		cells = Cell.all('wlan0')
-		for cell in cells:
-			if cell.address not in found:
-				print(datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + ": " + cell.address + " " + cell.ssid)
-				found.append(cell.address)
-				response = requests.get('https://xxx' + cell.address)
-				print(response.text)
-		time.sleep(1)
-	except Exception as e:
-		print(e)
-		time.sleep(5)
+        try:
+                print(datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + ": Scan...")
+                cells = Cell.all(scanning_interface)
+                for cell in cells:
+                        if cell.address not in found:
+                                print(datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + ": " + cell.address + " " + cell.ssid)
+                                found.append(cell.address)
+                                if bssid_area in cell.address:
+                                        response = requests.get('https://' + core_instance + '/pi-scan/?bssid=' + cell.address)
+                                        print(response.text)
+                                else:
+                                        print("*** Netzwerk nicht in Lokalisierungsbereich")
+                time.sleep(1)
+        except Exception as e:
+                print(e)
+                time.sleep(5)
