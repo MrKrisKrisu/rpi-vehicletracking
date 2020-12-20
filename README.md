685eeb1807adc4525f0fc7d542134ae865f275f2
Kris <git@k118.de>
2020-12-20 12:51:36 +0100

    Improved Script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vehicletracking Python Scanner
+
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/3142bfda3daf4429921c02517c0349f1)](https://www.codacy.com/gh/MrKrisKrisu/rpi-vehicletracking/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=MrKrisKrisu/rpi-vehicletracking&amp;utm_campaign=Badge_Grade)
--- script.py
+++ script.py
@@ -1,63 +1,109 @@
+#!/usr/bin/python3 -u
+
+import datetime
+import queue
+import threading
+import time
+
+import requests
+from wifi import Cell
+
 ############## CONFIG ###############
-core_instance="xxx.de"
-bssid_area="xx:xx:xx"
-scanning_interface="wlan0"
+core_instance = "xxx.de"
+bssid_area = []
+scanning_interface = "wlan0"
+token = ""
+telegram_token = "bot123:xxx"
+telegram_chat_id = ""
+scanner_name = ""
 #####################################
 
-from wifi import Cell
-import datetime
-import time
-import requests
-import os
-import sys
 
-found = []
+def message(message):
+    try:
+        requests.post('https://api.telegram.org/' + telegram_token + '/sendMessage',
+                      json={"chat_id": telegram_chat_id, 'text': message, 'parse_mode': 'HTML'})
+    except Exception as e:
+        print("Fehler beim Senden der Telegram-Nachricht")
+
+
+def saveThread(q):
+    time.sleep(5)
+    while True:
+        uploadData = q.get()
+        if len(uploadData) > 0:
+            try:
+                response = requests.post('https://' + core_instance + '/api/v1/scan', json=uploadData,
+                                         headers={'Authentication': token})
+                print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [SaveScan]: " + response.text)
+                json = response.json()
+                if json['data']['verified']:
+                    messageList = ""
+                    for vehicle in json['data']['verified']:
+                        messageList += "\r\n"
+                        messageList += str(vehicle['vehicle']) + "\r\n"
+                        messageList += "<i>" + str(vehicle['company']) + "</i>\r\n"
+                        messageList += "---------------";
+                    if messageList != "":
+                        message("<b><i>[" + scanner_name + "]</i> Fahrzeug lokalisiert</b>\r\n" + messageList)
+                if json['data']['unverified']:
+                    messageList = ""
+                    for vehicle in json['data']['unverified']:
+                        messageList += "- " + str(vehicle) + "\r\n";
+                    message(
+                        "<b><i>[" + scanner_name + "]</i> Unverifiziertes Fahrzeug lokalisiert</b>\r\n" + messageList)
+            except Exception as e:
+                print("Error on save. Retry.")
+                q.put(uploadData)
+
+
+def handleScan(qHandle, qSave):
+    while True:
+        cells = qHandle.get()
+        uploadData = []
+        for cell in cells:
+            if cell.address not in found:
+                found.append(cell.address)
+
+                if True:  # proceed:
+                    uploadData.append({
+                        'bssid': cell.address,
+                        'ssid': cell.ssid,
+                        'signal': cell.signal,
+                        'quality': cell.quality,
+                        'frequency': cell.frequency.replace(" GHz", ""),
+                        'encrypted': cell.encrypted == True,
+                        'channel': cell.channel,
+                        # 'latitude': 52.0,
+                        # 'longitude': 9.0,
+                        'created_at': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    })
+        qSave.put(uploadData)
+
+
+qSave = queue.Queue()
+qHandle = queue.Queue()
+
+saveT = threading.Thread(target=saveThread, args=(qSave,))
+saveT.start()
+
+handleT = threading.Thread(target=handleScan, args=(qHandle, qSave,))
+handleT.start()
+
 i = 0
-token = " "
-
-def init():
-	if os.path.isfile('/opt/rpi-vehicletracking/device_key'):
-		f = open('device_key', 'r')
-		token = f.readline()
-		f.close()
-		return token
-	else:
-		f = open('/opt/rpi-vehicletracking/device_key', "w+")
-		reqToken = requests.post('https://' + core_instance + '/api/scan/device/registernew').text
-		token = reqToken
-		f.write(token)
-		f.close()
-		return token
-
-token = init()
-
+found = []
 while True:
-        i += 1
-        if (i > 60 * 20):
-                found = []
-                i = 0
-        try:
-                print(datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + ": Scan...")
-                cells = Cell.all(scanning_interface)
-                for cell in cells:
-                        if cell.address not in found:
-                                print(datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + ": " + cell.address + " " + cell.ssid)
-                                found.append(cell.address)
-                                if bssid_area in cell.address:
-                                        response = requests.post('https://' + core_instance + '/api/scan', data = {
-                                                'bssid': cell.address,
-                                                'ssid': cell.ssid,
-                                                'signal': cell.signal,
-                                                'quality': cell.quality,
-                                                'frequency': cell.frequency,
-                                                'bitrates': cell.bitrates,
-                                                'encrypted': cell.encrypted,
-                                                'channel': cell.channel
-                                        }, headers={'X-Api-Token': token})
-                                        print(response.text)
-                                else:
-                                        print("*** Netzwerk nicht in Lokalisierungsbereich")
-                time.sleep(1)
-        except Exception as e:
-                print(e)
-                time.sleep(5)
+    i += 1
+    if i > 60 * 10:
+        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [Main]: Laufzeit erreicht.")
+        i = 1
+        found = []
+    try:
+        cells = Cell.all(scanning_interface)
+        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [Main]: Scan... (" + str(
+            qHandle.qsize()) + " Scans zu verarbeiten, )")
+        qHandle.put(cells)
+        time.sleep(0.2)
+    except Exception as e:
+        print(e)
+        time.sleep(0.1)
