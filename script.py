f6bebb3a673c4f87d4072e68b30f0847dc1013a2
Kris <git@k118.de>
2021-08-22 01:50:36 +0200

    Improve script (#1)
    * Improvements
    
    * Improvements
    
    * Improvements
    
    * Improvements
    
    * Improvements
    
    * Improvements
    
    * Improvements
    
    * Improvements

--- script.py
+++ script.py
@@ -4,57 +4,68 @@ import datetime
 import queue
 import threading
 import time
-
+import configparser
 import requests
+import socket
 from wifi import Cell
 
-############## CONFIG ###############
-core_instance = "xxx.de"
-bssid_area = []
-scanning_interface = "wlan0"
-token = ""
-telegram_token = "bot123:xxx"
-telegram_chat_id = ""
-scanner_name = ""
-#####################################
+config = configparser.ConfigParser()
+config.sections()
+config.read('config.ini')
+
+verbose = config['SCRIPT']['verbose']
+core_instance = config['SERVER']['hostname']
+scanning_interface = config['SCAN']['wifi_interface']
+token = config['SERVER']['token']
+scan_interval = float(config['SCAN']['scan_interval'])
 
 
-def message(message):
+def log(thread, message, force=False):
+    if verbose == "true" or force:
+        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [" + thread + "]: " + message)
+
+
+log("Main", "Starting script...", True)
+log("Main", "____________________________________________", True)
+log("Main", "Scanning interface " + scanning_interface, True)
+log("Main", "Scanning every " + str(scan_interval) + " seconds", True)
+log("Main", "Pushing to Host " + core_instance, True)
+log("Main", "Logging is active? -> " + verbose, True)
+log("Main", "____________________________________________", True)
+
+
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
     try:
-        requests.post('https://api.telegram.org/' + telegram_token + '/sendMessage',
-                      json={"chat_id": telegram_chat_id, 'text': message, 'parse_mode': 'HTML'})
-    except Exception as e:
-        print("Fehler beim Senden der Telegram-Nachricht")
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
 
 
 def saveThread(q):
     time.sleep(5)
     while True:
-        uploadData = q.get()
-        if len(uploadData) > 0:
-            try:
-                response = requests.post('https://' + core_instance + '/api/v1/scan', json=uploadData,
-                                         headers={'Authentication': token})
-                print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [SaveScan]: " + response.text)
-                json = response.json()
-                if json['data']['verified']:
-                    messageList = ""
-                    for vehicle in json['data']['verified']:
-                        messageList += "\r\n"
-                        messageList += str(vehicle['vehicle']) + "\r\n"
-                        messageList += "<i>" + str(vehicle['company']) + "</i>\r\n"
-                        messageList += "---------------";
-                    if messageList != "":
-                        message("<b><i>[" + scanner_name + "]</i> Fahrzeug lokalisiert</b>\r\n" + messageList)
-                if json['data']['unverified']:
-                    messageList = ""
-                    for vehicle in json['data']['unverified']:
-                        messageList += "- " + str(vehicle) + "\r\n";
-                    message(
-                        "<b><i>[" + scanner_name + "]</i> Unverifiziertes Fahrzeug lokalisiert</b>\r\n" + messageList)
-            except Exception as e:
-                print("Error on save. Retry.")
-                q.put(uploadData)
+        log("SaveThread", str(q.qsize()) + " Uploads pending...")
+        if internet():
+            uploadData = q.get()
+            if len(uploadData) > 0:
+                try:
+                    response = requests.post('https://' + core_instance + '/api/v1/scan', json=uploadData,
+                                             headers={'Authentication': token})
+                    log("SaveScan", response.text)
+                except Exception:
+                    log("SaveThread", "Error on save. Retry.", True)
+                    q.put(uploadData)
+                time.sleep(0.5)
+        else:
+            time.sleep(5)
 
 
 def handleScan(qHandle, qSave):
@@ -63,21 +74,18 @@ def handleScan(qHandle, qSave):
         uploadData = []
         for cell in cells:
             if cell.address not in found:
+                log("ScanThread", "Found new network " + cell.address + " / " + cell.ssid)
                 found.append(cell.address)
-
-                if True:  # proceed:
-                    uploadData.append({
-                        'bssid': cell.address,
-                        'ssid': cell.ssid,
-                        'signal': cell.signal,
-                        'quality': cell.quality,
-                        'frequency': cell.frequency.replace(" GHz", ""),
-                        'encrypted': cell.encrypted == True,
-                        'channel': cell.channel,
-                        # 'latitude': 52.0,
-                        # 'longitude': 9.0,
-                        'created_at': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    })
+                uploadData.append({
+                    'bssid': cell.address,
+                    'ssid': cell.ssid,
+                    'signal': cell.signal,
+                    'quality': cell.quality,
+                    'frequency': cell.frequency.replace(" GHz", ""),
+                    'encrypted': cell.encrypted == True,
+                    'channel': cell.channel,
+                    'created_at': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                })
         qSave.put(uploadData)
 
 
@@ -95,15 +103,14 @@ found = []
 while True:
     i += 1
     if i > 60 * 10:
-        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [Main]: Laufzeit erreicht.")
+        log("Main", "Laufzeit erreicht.")
         i = 1
         found = []
     try:
+        log("Main", "Scan...")
         cells = Cell.all(scanning_interface)
-        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [Main]: Scan... (" + str(
-            qHandle.qsize()) + " Scans zu verarbeiten, )")
         qHandle.put(cells)
-        time.sleep(0.2)
+        time.sleep(scan_interval)
     except Exception as e:
-        print(e)
+        log("Main", "Error: " + str(e), True)
         time.sleep(0.1)
