dd17b8341d3bf57672e904a36b11378416714ce2
Kristian Stöckel <git@k118.de>
2021-08-22 00:32:02 +0200

    Improvements

--- script.py
+++ script.py
@@ -12,20 +12,15 @@ config = configparser.ConfigParser()
 config.sections()
 config.read('config.ini')
 
-############## CONFIG ###############
 verbose = config['SCRIPT']['verbose']
 core_instance = config['SERVER']['hostname']
-bssid_area = []
 scanning_interface = config['SCAN']['wifi_interface']
 token = config['SERVER']['token']
-telegram_token = "bot123:xxx"
-telegram_chat_id = ""
-scanner_name = ""
-#####################################
 
-if verbose == "true":
-    print("Jaaaa")
-print(verbose)
+
+def verbose(thread, message):
+    if verbose == "true":
+        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [" + thread + "]: " + message)
 
 
 def saveThread(q):
@@ -36,7 +31,7 @@ def saveThread(q):
             try:
                 response = requests.post('https://' + core_instance + '/api/v1/scan', json=uploadData,
                                          headers={'Authentication': token})
-                print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [SaveScan]: " + response.text)
+                verbose("SaveScan", response.text)
             except Exception:
                 print("Error on save. Retry.")
                 q.put(uploadData)
@@ -49,20 +44,16 @@ def handleScan(qHandle, qSave):
         for cell in cells:
             if cell.address not in found:
                 found.append(cell.address)
-
-                if True:  # proceed:
-                    uploadData.append({
-                        'bssid': cell.address,
-                        'ssid': cell.ssid,
-                        'signal': cell.signal,
-                        'quality': cell.quality,
-                        'frequency': cell.frequency.replace(" GHz", ""),
-                        'encrypted': cell.encrypted == True,
-                        'channel': cell.channel,
-                        # 'latitude': 52.0,
-                        # 'longitude': 9.0,
-                        'created_at': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    })
+                uploadData.append({
+                    'bssid': cell.address,
+                    'ssid': cell.ssid,
+                    'signal': cell.signal,
+                    'quality': cell.quality,
+                    'frequency': cell.frequency.replace(" GHz", ""),
+                    'encrypted': cell.encrypted == True,
+                    'channel': cell.channel,
+                    'created_at': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                })
         qSave.put(uploadData)
 
 
@@ -80,13 +71,12 @@ found = []
 while True:
     i += 1
     if i > 60 * 10:
-        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [Main]: Laufzeit erreicht.")
+        verbose("Main", "Laufzeit erreicht.")
         i = 1
         found = []
     try:
         cells = Cell.all(scanning_interface)
-        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " [Main]: Scan... (" + str(
-            qHandle.qsize()) + " Scans zu verarbeiten, )")
+        verbose("Main", "Scan... (" + str(qHandle.qsize()) + " Scans zu verarbeiten, )")
         qHandle.put(cells)
         time.sleep(0.2)
     except Exception as e:
